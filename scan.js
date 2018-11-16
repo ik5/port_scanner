493d46647e2a80b669c0fdb3780c606b542663d2
Ido Kanner <idok@linesip.com>
2018-11-16 19:43:27 +0200

    nodejs scanner

---
+++ scan.js
@@ -0,0 +1,37 @@
+/*jshint esversion: 6 */
+
+function scan(address, port, timeout, status) {
+  const net = require('net');
+  const dns = require('dns');
+
+  dns.lookup(address, (err, host, family) => {
+    if (err) {
+      status('Resolved error');
+      return;
+    }
+
+    const client = net.createConnection({host, port}, () => {
+      status('open');
+    });
+    client.on('error', (err) => {
+      switch (err.code) {
+        case 'ECONNREFUSED':
+          status('filtered');
+          break;
+        default:
+          status(err);
+          break;
+      }
+    });
+    client.setTimeout(timeout * 1000, () => {
+      if (client) {
+        client.destroy();
+      } else {
+        status('closed');
+      }
+    });
+  });
+
+}
+
+scan('google.com', 443, 3, (status) => console.log(status));
